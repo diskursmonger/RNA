35b1a2d393063788520e59ea376659b93c8f3a30
diskursmonger <57355810+diskursmonger@users.noreply.github.com>
2019-11-21 18:12:15 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,18 +12,14 @@ int main() {
     testing::InitGoogleTest();
     RUN_ALL_TESTS();
     RNA r4;
-    RNA r5(1, T);
-    for (int i = 1; i <= 1000000; i++) {
-        // r4 = r4 + r5;
-        r4.add(T);
-        if (i % 2000 == 0) {
-            // std::cout << i << std::endl;
-            std::cout << r4[i] << std::endl;
-        }
-    }
+    RNA r5(10, T);
     r4.get_length();
-    // Nucleotide Nuclec = r4.get(1000000);
-    // std::cout << Nuclec << std::endl;
+    r4 = r4 + r5;
+    r4.get_length();
+    for (int i = 1; i < 11; i++) {
+        Nucleotide Nuclec = r4.get(i);
+        std::cout << Nuclec << std::endl;
+    }
     times(&end);
     clocks = end.tms_utime - start.tms_utime;
     printf("Time taken: %lf sec.\n", (double) clocks / clocks_per_sec);
